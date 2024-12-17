5a093d8b1304b97253ef85bdd6c7bba9970f5a8b
Jeffrey <166993448+jffryyy@users.noreply.github.com>
2024-12-17 23:55:17 +0800

    Update settings.py

--- wasteredux_site/wasteredux_site/settings.py
+++ wasteredux_site/wasteredux_site/settings.py
@@ -15,11 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'localhost', 
-    '127.0.0.1', 
-    '.vercel.app',
-    ]  # Update with actual hostnames
+ALLOWED_HOSTS = ['*']
 
 
 
